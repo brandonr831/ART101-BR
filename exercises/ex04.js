da7bb187dd823440a1a8ac360a76987e12456f6d
brandonr831 <bulysses127@gmail.com>
2026-04-27 11:16:56 -0700

    update

---
+++ exercises/ex04.js
@@ -0,0 +1,15 @@
+
+let count=0;
+
+$("#needy-button").click( function() {
+    
+    count= count +1;
+    $("#needy-button").html("YOU'RE GETTING $" +
+      count)
+    
+    
+    
+    
+    });
+
+
