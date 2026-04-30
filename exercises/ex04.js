e80666a0eac6edf18f0b6562f452ef62c53ae064
brandonr831 <bulysses127@gmail.com>
2026-04-30 10:24:42 -0700

    y

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,15 +1,28 @@
-
 let count=0;
+let colors=["Orchid", "Coral", "HotPink", "Plum"];
+let colorCount;
 
-$("#needy-button").click( function() {
-    
-    count= count +1;
-    $("#needy-button").html("YOU'RE GETTING $" +
-      count)
-    
-    
-    
-    
-    });
+let pencil={
+color: "Yellow",
+isSharp: false,
+};
 
+$("#needy-button").click( function(){
 
+    count = count +1; 
+
+    colorCount=count-1;
+
+    $("#needy-button").html("you clicked me " + count + " and your favourite color today is: " + colors[colorCount] );
+
+    $("#needy-button").css( "background-color", colors[colorCount]);  
+
+    $("html").css( "background-color", colors[colorCount+1]);
+
+    console.log(pencil.color);
+
+// things to try out:
+// make the page background color two colors ahead of the button color
+// make the page background color one color beyond of the button color
+
+});``
