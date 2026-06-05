01e63c1ffcc26828c5e19b3acd1d4e29fe97024b
brandonr831 <bulysses127@gmail.com>
2026-06-05 12:56:55 -0700

    y

---
+++ environ/env04.js
@@ -0,0 +1,72 @@
+let workoutCount = 0;
+
+let gymName = "Brandon's Gym";
+
+let equipment = ["dumbbell", "glasses", "old weights", "bench"];
+
+let jack = {
+    name: "Jack",
+    type: "gym rat",
+    mood: "aggressive",
+    favorite: "dumbbell"
+};
+
+$("#gymName").click(function () {
+    $("#final").html("<p>This gym is called " + gymName + "</p>");
+});
+
+$("#equipment").click(function () {
+    $("#final").html("<p>Equipment: " + equipment + "</p>");
+});
+
+$("#jacked").click(function () {
+    $("#final").html(
+        "<p>Name: " + jack.name + "</p>" +
+        "<p>Type: " + jack.type + "</p>" +
+        "<p>Mood: " + jack.mood + "</p>" +
+        "<p>Favorite item: " + jack.favorite + "</p>"
+    );
+});
+
+$("#workoutCounter").click(function () {
+    workoutCount = workoutCount + 1;
+
+    $("#final").html(
+        "<p>You worked out " + workoutCount + " times.</p>"
+    );
+});
+
+function tuneGym(mood) {
+
+    if (mood == "intense") {
+
+        $("body").css("background-image", "url('gymroom.jpeg')");
+
+        $("#final").html(
+            "<p>The gym feels intense time for a sick pump</p>"
+        );
+
+    } else if (mood == "calm") {
+
+        $("body").css("background-image", "url('day.jpeg')");
+
+        $("#final").html(
+            "<p>The gym feels calm time for cardio</p>"
+        );
+
+    } else {
+
+        $("#final").html(
+            "<p>Please type 'calm' or 'intense'.</p>"
+        );
+
+    }
+}
+
+$("#tuneGym").click(function () {
+
+    let answer = prompt("Type calm or intense");
+
+    tuneGym(answer);
+
+});
--- exercises/lab05.js
+++ exercises/lab05.js
@@ -11,4 +11,4 @@ function askNumber(whatNumber) {
 
 $("#good-button").click(function () {
     askNumber(5);
-});
+}); 
